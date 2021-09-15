7e7b613dc3ec3e2d6610c6e6281d987c618b0ea1
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-09-16 00:21:50 +0200

    titlle fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = '#RegisterToVoteZW';
+ // title = '#RegisterToVoteZW';
 
   constructor(private metaTagService: Meta) {}
 
