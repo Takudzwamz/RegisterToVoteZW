9d27058ce277684125ba70ed8eb24e8be09d9ce3
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-11-01 14:43:49 +0200

    new domain name change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,50 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ArticleDetailsComponent } from './article-details/article-details.component';
-import { ArticlesComponent } from './articles/articles.component';
-import { CategoryComponent } from './category/category.component';
-import { CentersComponent } from './centers/centers.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { HomeComponent } from './home/home.component';
-import { PoliciesComponent } from './policies/policies.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ArticleDetailsComponent } from "./article-details/article-details.component";
+import { ArticlesComponent } from "./articles/articles.component";
+import { CategoryComponent } from "./category/category.component";
+import { CentersComponent } from "./centers/centers.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { HomeComponent } from "./home/home.component";
+import { PoliciesComponent } from "./policies/policies.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent,
   },
   {
-    path: 'why_voting',
+    path: "why_voting",
     component: PoliciesComponent,
   },
   {
-    path: 'centers',
+    path: "centers",
     component: CentersComponent,
   },
   {
-    path: 'news',
+    path: "news",
     component: ArticlesComponent,
   },
+
+  { path: "article/:id", 
+    component: ArticleDetailsComponent 
+  },
+  { path: "news-category/:id", 
+    component: CategoryComponent 
+  },
   {
-    path: 'contact_us',
+    path: "contact_us",
     component: ContactUsComponent,
   },
-  { path: 'article/:id', component: ArticleDetailsComponent },
-  { path: 'news-category/:id', component: CategoryComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', initialNavigation: 'enabled' })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      relativeLinkResolution: "legacy",
+      initialNavigation: "enabled",
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
